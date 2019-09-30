f495af0d6aa43337ef7af6e098904e567bb70704
Jhony A. Gonzalez C <jhonyalexanderg@hotmail.com>
2019-09-30 16:57:03 -0500

    Creación del Formulario Horarios

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TypographyComponent implements OnInit {
 
+data: any = {};
+arrayEstado = [{'nombre': 'Activo', 'id': 1}, {'nombre': 'Inactivo', 'id': 2}];
+arrayHorario = [
+    {'horaInicio': '08:00 AM', 'horaFin': '09:00 AM', 'estado': 'Activo'},
+    {'horaInicio': '09:00 AM', 'horaFin': '10:00 AM', 'estado': 'Activo'},
+    {'horaInicio': '10:00 AM', 'horaFin': '11:00 AM', 'estado': 'Activo'},
+    {'horaInicio': '11:00 AM', 'horaFin': '12:00 PM', 'estado': 'Activo'},
+    {'horaInicio': '12:00 PM', 'horaFin': '13:00 PM', 'estado': 'Activo'}
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  guardar(){
+
+    console.log(this.data);
+
   }
 
 }
--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -6,16 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
-data:any={};
-arrayPerfil=[{'nombre':'Administrador', 'id':2}, {'nombre':'Usuario', 'id':3}];
-arrayEstado = [{'nombre': 'Activo', 'id': 1},{'nombre': 'Inactivo', 'id': 2}];
-arrayUsuario=[
-    {'numeroDocumento':1053893331,'nombre':'Juan Carlos','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Administrador','estado':'Activo'},
-    {'numeroDocumento':1053893332,'nombre':'David camilo','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Usuario','estado':'Activo'},
-    {'numeroDocumento':1053893333,'nombre':'Esteban','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Usuario','estado':'Activo'},
-    {'numeroDocumento':1053893334,'nombre':'Camilo andres','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Usuario','estado':'Acitvo'},
-    {'numeroDocumento':1053893335,'nombre':'Mario felipe','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Usuario','estado':'Activo'},
-    {'numeroDocumento':1053893336,'nombre':'Marlon','direccion':'Calle 32 #43-1','telefono':8769976,'perfil':'Usuario','estado':'Activo'}
+data: any = {};
+arrayPerfil = [{'nombre': 'Administrador', 'id': 2}, {'nombre': 'Usuario', 'id': 3}];
+arrayEstado = [{'nombre': 'Activo', 'id': 1}, {'nombre': 'Inactivo', 'id': 2}];
+arrayUsuario = [
+    {'numeroDocumento': 1053893331, 'nombre': 'Juan Carlos', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Administrador', 'estado': 'Activo'},
+    {'numeroDocumento': 1053893332, 'nombre': 'David camilo', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Usuario', 'estado': 'Activo'},
+    {'numeroDocumento': 1053893333, 'nombre': 'Esteban', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Usuario', 'estado': 'Activo'},
+    {'numeroDocumento': 1053893334, 'nombre': 'Camilo andres', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Usuario', 'estado': 'Acitvo'},
+    {'numeroDocumento': 1053893335, 'nombre': 'Mario felipe', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Usuario', 'estado': 'Activo'},
+    {'numeroDocumento': 1053893336, 'nombre': 'Marlon', 'direccion': 'Calle 32 #43-1', 'telefono': 8769976, 'perfil': 'Usuario', 'estado': 'Activo'}
   ];
   constructor() { }
 
